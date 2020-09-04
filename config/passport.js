2e646e678c3180825b980b7663d8fa242e921f99
Brianna Fisher <Bnfisher4@gmail.com>
2020-09-04 10:44:00 -0500

    set up login/logout oauth

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,40 @@
 var passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+},
+    function (accessToken, refreshToken, profile, cb) {
+        // a user has logged in with OAuth...
+        User.findOne({ 'googleId': profile.id }, function (err, user) {
+            if (err) return cb(err);
+            if (user) {
+                return cb(null, user);
+            } else {
+                // we have a new user via OAuth!
+                const newuser = new User({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleId: profile.id
+                });
+                newuser.save(function (err) {
+                    if (err) return cb(err);
+                    return cb(null, newuser);
+                });
+            }
+        });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const indexCtrl = require('../controllers');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get('/users', indexCtrl.index);
+
+
 
 module.exports = router;
