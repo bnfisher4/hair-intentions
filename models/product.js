10108b67605d360e71541507f62156efe90e3ba1
Brianna Fisher <Bnfisher4@gmail.com>
2020-09-07 11:19:08 -0500

    creates product model

--- models/product.js
+++ models/product.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-
+    img: {
+        type: String
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    kind: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Product', productSchema);
